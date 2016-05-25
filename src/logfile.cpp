5941c2bbb216b70002eeaada9fafa7c6e7e4730c
Mark Ludwig <mk.ludwig1@gmail.com>
2016-05-24 21:21:51 -0700

    Created LogFileStream class; Updated LogFile methods

--- src/logfile.cpp
+++ src/logfile.cpp
@@ -1,72 +1,107 @@
 #include "logfile.h"
 
-namespace LogFile {
-    bool bCreateLog = true;
+LogFileStream::LogFileStream(LPCSTR filename, bool append)
+{
+    debugf("LogFileStream::LogFileStream(\"%s\", %s)", filename, (append) ? "true" : "false");
+    m_filename = filename;
+    this->SetAppendMode(append);
+}
 
-    bool bAppendLine = false;
-    bool bIgnoreBuffer = false;
+void LogFileStream::Close(void)
+{
+    if (m_file != NULL)
+        fclose(m_file);
+}
 
-    void Write(LPCSTR str)
-    {
-        if (!bIgnoreBuffer)
-        {
-            // also output a debug message
-            OutputDebugStringA(str);
-        }
-
-        FILE *logFile = fopen("mm2hook.log", ((bCreateLog) ? "w" : "a+"));
-
-        if (logFile == NULL)
-        {
-            debug("Failed to open the log file!");
-            return;
-        }
-
-        if (bCreateLog)
-        {
-            fprintf(logFile, "--<< MM2Hook log file >>--\n\n");
-            bCreateLog = false;
-        }
-
-        if (!bIgnoreBuffer)
-        {
-            int strLen = strlen(str);
-
-            if (strLen > 0)
-                fwrite(str, 1, strLen, logFile);
-        }
-
-        if (bAppendLine)
-        {
-            fprintf(logFile, "\n");
-            bAppendLine = false; // disable since we're done
-        }
-
-        fclose(logFile);
-    }
-
-    void WriteLine(LPCSTR str)
-    {
-        bAppendLine = true; // disabled after use
-        Write(str);
-    }
-
-    void Format(LPCSTR format, ...)
-    {
-        char buf[__LOGFMT_BUF_SIZE];
-        va_list va;
-        va_start(va, format);
-        _vsnprintf(buf, sizeof(buf), format, va);
-        va_end(va);
-
-        Write(buf);
-    }
-
-    void AppendLine(void)
-    {
-        bAppendLine = true;
-        bIgnoreBuffer = true;
-        Write(NULL);
-        bIgnoreBuffer = false;
+void LogFileStream::SetAppendMode(bool append)
+{
+    if ((m_file = fopen(m_filename, (append) ? "a+" : "w")) == NULL) {
+        debug((append) ? "Failed to open the log file!" : "Failed to create the log file!");
     }
 }
+
+LogFileStream* LogFileStream::Create(LPCSTR filename, LPCSTR title)
+{
+    auto log = new LogFileStream(filename);
+
+    if (title != NULL)
+        log->WriteLine(title);
+
+    log->SetAppendMode(true);
+
+    return log;
+}
+
+LogFileStream* LogFileStream::Create(LPCSTR filename)
+{
+    return LogFileStream::Create(filename, NULL);
+}
+
+LogFileStream* LogFileStream::Open(LPCSTR filename)
+{
+    return new LogFileStream(filename, true);
+}
+
+void LogFileStream::AppendLine(void)
+{
+    fprintf(m_file, "\n");
+    fflush(m_file);
+}
+
+void LogFileStream::Write(LPCSTR str)
+{
+#ifndef LOGFILE_NO_DEBUG
+    // also output a debug message
+    OutputDebugStringA(str);
+#endif
+
+    int strLen = strlen(str);
+
+    if (strLen > 0)
+    {
+        fwrite(str, 1, strLen, m_file);
+        fflush(m_file);
+    }
+}
+
+void LogFileStream::WriteLine(LPCSTR str)
+{
+    Write(str);
+    AppendLine();
+}
+
+LogFileStream *g_logfile = NULL;
+char g_logfile_buffer[__LOGFMT_BUF_SIZE] = { NULL };
+
+void LogFile::Initialize(LPCSTR filename) {
+    g_logfile = LogFileStream::Create(filename);
+};
+
+void LogFile::Initialize(LPCSTR filename, LPCSTR title) {
+    g_logfile = LogFileStream::Create(filename, title);
+};
+
+void LogFile::Close(void) {
+    g_logfile->Close();
+};
+
+void LogFile::AppendLine(void) {
+    g_logfile->AppendLine();
+};
+
+void LogFile::Write(LPCSTR str) {
+    g_logfile->Write(str);
+};
+
+void LogFile::WriteLine(LPCSTR str) {
+    g_logfile->WriteLine(str);
+};
+
+void LogFile::Format(LPCSTR format, ...) {
+    va_list va;
+    va_start(va, format);
+    vsprintf(g_logfile_buffer, format, va);
+    va_end(va);
+
+    g_logfile->Write(g_logfile_buffer);
+};
--- src/logfile.h
+++ src/logfile.h
@@ -7,16 +7,46 @@
 
 #define __LOGFMT_BUF_SIZE 1024
 
-namespace LogFile {
-    extern bool bCreateLog;
+class LogFileStream {
+protected:
+    LPCSTR m_filename;
+    FILE *m_file;
 
-    extern bool bAppendLine;
-    extern bool bIgnoreBuffer;
+    LogFileStream(LPCSTR filename, bool append = false);
+
+    void SetAppendMode(bool append);
+public:
+    static LogFileStream* Create(LPCSTR filename, LPCSTR title);
+    static LogFileStream* Create(LPCSTR filename);
+
+    static LogFileStream* Open(LPCSTR filename);
+
+    void Close(void);
+
+    void AppendLine(void);
+
+    void Write(LPCSTR str);
+    void WriteLine(LPCSTR str);
+
+    template <typename ...TArgs>
+    __inline void Format(LPCSTR format, TArgs ...args) {
+        char buf[__LOGFMT_BUF_SIZE];
+        sprintf_s(buf, sizeof(buf), format, args...);
+        
+        Write(buf);
+    };
+};
+
+namespace LogFile {
+    void Initialize(LPCSTR filename);
+    void Initialize(LPCSTR filename, LPCSTR title);
+
+    void Close(void);
+
+    void AppendLine(void);
 
     void Write(LPCSTR str);
     void WriteLine(LPCSTR str);
 
     void Format(LPCSTR format, ...);
-
-    void AppendLine(void);
-}
+};
