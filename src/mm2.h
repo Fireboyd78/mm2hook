d9e532b25738ae4d615a97ab1b8d32488bbfe5ed
Mark Ludwig <mk.ludwig1@gmail.com>
2016-09-06 01:31:57 -0700

    Add cityTimeWeatherLighting struct

--- src/mm2.h
+++ src/mm2.h
@@ -328,6 +328,28 @@ namespace MM2 {
         float Z;
         float W;
     };
+
+    struct cityTimeWeatherLighting {
+        /*0x00*/float KeyHeading;
+        /*0x04*/float KeyPitch;
+        /*0x08*/Vector3 KeyColor;
+
+        /*0x14*/float Fill1Heading;
+        /*0x18*/float Fill1Pitch;
+        /*0x1C*/Vector3 Fill1Color;
+
+        /*0x28*/float Fill2Heading;
+        /*0x2C*/float Fill2Pitch;
+        /*0x30*/Vector3 Fill2Color;
+
+        // "ambient light levels" that get calculated using the Ambient color?
+        // doesn't care for any of the Key/Pitch values
+        /*0x3C*/int AmbientLevel3; // fullbright
+        /*0x40*/int AmbientLevel2;
+        /*0x44*/int AmbientLevel1;
+
+        /*0x48*/int Ambient; // looks like the alpha is ignored
+    };
 };
 
 class CMidtownMadness2 : public CAGEGame {
