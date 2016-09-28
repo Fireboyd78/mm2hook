16d76e4abae4c73dc3160d1ebfbd23cd1baf61f9
Mark Ludwig <mk.ludwig1@gmail.com>
2016-09-27 18:55:18 -0700

    Enabled some useful warnings that were off by default.

--- src/common.h
+++ src/common.h
@@ -23,5 +23,15 @@
   #include <LuaIntf/LuaIntf.h>
 #endif // !_LUA_EMBED
 
-//  warning C4091: '__declspec(novtable)': ignored on left of 'x' when no variable is declared
+//  C4091: '__declspec(novtable)': ignored on left of 'x' when no variable is declared
 #pragma warning (disable : 4091)
+
+/*
+    Enable some warnings off by default
+
+    C4710: "'function' : function not inlined"
+    C4711: "function 'function' selected for inline expansion"
+    C4714: "function 'function' marked as __forceinline not inlined"
+*/
+#pragma warning (1 : 4711)
+#pragma warning (3 : 4710 4714)
