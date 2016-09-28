812bd7cb5f75d68dee4fdaac529457e08bed1d5a
Mark Ludwig <mk.ludwig1@gmail.com>
2016-09-27 19:08:03 -0700

    Re-disabled warnings (no wonder they're disabled)

--- src/common.h
+++ src/common.h
@@ -25,13 +25,3 @@
 
 //  C4091: '__declspec(novtable)': ignored on left of 'x' when no variable is declared
 #pragma warning (disable : 4091)
-
-/*
-    Enable some warnings off by default
-
-    C4710: "'function' : function not inlined"
-    C4711: "function 'function' selected for inline expansion"
-    C4714: "function 'function' marked as __forceinline not inlined"
-*/
-#pragma warning (1 : 4711)
-#pragma warning (3 : 4710 4714)
