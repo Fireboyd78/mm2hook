6979d2e9a1fdab5a5384ad244236f4e8a0a3e5a4
Brick <samuelwilde@hotmail.com>
2016-10-15 14:58:06 +0100

    Added wall of text regarding BridgeFerryHandler

--- src/main.cpp
+++ src/main.cpp
@@ -223,6 +223,20 @@ public:
     }
 };
 
+/*
+    By default, bridges are treated as "Cullables" instead of "Drawables" (MM2 is wierd)
+
+    Before the patch, Call calls dgBangerInstance::Draw, and Draw does nothing.
+    With the patch, these 2 functions are swapped around, so Draw calls dgBangerInstance::Draw, and Cull does nothing.
+
+    Problem solved, right? Nope. Cull is called by gizBridgeMgr::Cull, where as Draw is called by cityLevel::DrawRooms.
+
+    gizBridgeMgr has it's own maximum draw distance for bridges, so gizBridgeMgr::Cull draws them fine at range.
+    But cityLevel::DrawRooms uses the prop lod distances, so the bridges end up disappearing a lot sooner.
+
+    A "solution" is to manually set the LOD distance values (see cityLevel::SetObjectDetail).
+    But that would cause everything to be drawn further, and decrase FPS.
+*/
 class BridgeFerryHandler {
 public:
     void Cull(int lod) {
