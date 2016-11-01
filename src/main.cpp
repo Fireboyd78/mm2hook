0272764b60767563909cda1f6d877caf6a00d3d4
Diatosta <diatosta123@gmail.com>
2016-11-01 19:59:52 +0000

    Fixes crash when trying to open LUA console in the main menu.

--- src/main.cpp
+++ src/main.cpp
@@ -90,6 +90,8 @@ AGEHook<0x62B068>::Type<int> timeOfDay;
 
 AGEHook<0x627518>::Type<int> carLights;
 
+AGEHook<0x6B2E41>::Type<int> gameState;   //0 = menu, 1 = level loaded
+
 /*
     ===========================================================================
 */
@@ -105,23 +107,27 @@ bool HandleKeyPress(DWORD vKey)
         // '`'
         case VK_OEM_8:
         {
-            // tell the game to open a chat box,
-            // and then use a local variable to check if it's open
+            // check if the level is loaded
+            if (*gameState == 1) {
 
-            mmGameManager *mgr = mmGameManager::Instance();
-            auto gamePtr = mgr->getGame();
+                // tell the game to open a chat box,
+                // and then use a local variable to check if it's open
 
-            if (gamePtr != NULL)
-            {
-                auto popup = gamePtr->getPopup();
+                mmGameManager *mgr = mmGameManager::Instance();
+                auto gamePtr = mgr->getGame();
 
-                if (popup != NULL) {
-                    // don't try opening it again if it's already open
-                    if (popup->IsEnabled() && isConsoleOpen)
-                        return true;
+                if (gamePtr != NULL)
+                {
+                    auto popup = gamePtr->getPopup();
 
-                    popup->ProcessChat();
-                    isConsoleOpen = true;
+                    if (popup != NULL) {
+                        // don't try opening it again if it's already open
+                        if (popup->IsEnabled() && isConsoleOpen)
+                            return true;
+
+                        popup->ProcessChat();
+                        isConsoleOpen = true;
+                    }
                 }
             }
         } return true;
