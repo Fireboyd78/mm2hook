1d89c5de832e097494740a4922cbf7601e8243b0
Mark Ludwig <mk.ludwig1@gmail.com>
2017-11-19 03:09:49 -0800

    Added mmVehInfo and mmVehList classes.

--- src/mm2_vehicle.h
+++ src/mm2_vehicle.h
@@ -7,9 +7,81 @@
 namespace MM2
 {
     // Forward declarations
+    class mmVehInfo;
+    class mmVehList;
     class vehCar;
     class vehCarAudioContainer;
     class vehPoliceCarAudio;
+
+    class mmVehInfo {
+    private:
+        char baseName[40];
+        char description[80];
+        char colors[64];
+        int flags;
+        int order;
+        BOOL valid;
+        int unlockScore;
+        int unlockFlags;
+        int unk_F4;
+        int horsepower;
+        int topSpeed;
+        int durability;
+        int mass;
+        int unk_108;
+        int unk_10C;
+        int scoringBias;
+        float uiDist;
+        float unk_118;
+        float unk_11C;
+        unsigned long tuningCRC;
+    public:
+        AGE_API mmVehInfo(void) {
+            PUSH_VTABLE();
+            ageHook::Thunk<0x529720>::Call<void>(this);
+            POP_VTABLE();
+        }
+
+        virtual AGE_API ~mmVehInfo(void) {
+            PUSH_VTABLE();
+            ageHook::Thunk<0x529770>::Call<void>(this);
+            POP_VTABLE();
+        }
+
+        inline char * GetBaseName(void) {
+            return baseName;
+        }
+
+        inline char * GetDescription(void) {
+            return description;
+        }
+    };
+
+    class mmVehList {
+    private:
+        mmVehInfo **vehInfos;
+        mmVehInfo *defaultVehicle;
+        int numVehicles;
+    public:
+        AGE_API mmVehList(void) {
+            PUSH_VTABLE();
+            ageHook::Thunk<0x524550>::Call<void>(this);
+            POP_VTABLE();
+        }
+
+        virtual AGE_API ~mmVehList(void) {
+            PUSH_VTABLE();
+            ageHook::Thunk<0x524570>::Call<void>(this);
+            POP_VTABLE();
+        }
+
+        AGE_API int GetVehicleID(char *vehicle)             { return ageHook::Thunk<0x5246B0>::Call<int>(this, vehicle); }
+
+        AGE_API mmVehInfo * GetVehicleInfo(int vehicle)     { return ageHook::Thunk<0x5245E0>::Call<mmVehInfo *>(this, vehicle); }
+        AGE_API mmVehInfo * GetVehicleInfo(char *vehicle)   { return ageHook::Thunk<0x524610>::Call<mmVehInfo *>(this, vehicle); }
+    };
+
+    declhook(0x6B1CA8, _Type<mmVehList *>, VehicleListPtr);
 
     class vehCarModel : public lvlInstance {
     protected:
