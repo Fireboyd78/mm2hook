caa023a2d4051c11f3d786c1a60925273e7ad63a
Fireboyd78 <mk.ludwig1@gmail.com>
2018-05-03 02:56:32 -0700

    Some more code cleanup.

--- src/mm2_vehicle.h
+++ src/mm2_vehicle.h
@@ -103,32 +103,32 @@ namespace MM2
         AGE_API void ClearDamage()                          { ageHook::Thunk<0x4CDFF0>::Call<void>(this); }
         AGE_API void EjectOneshot()                         { ageHook::Thunk<0x4CDCA0>::Call<void>(this); }
         AGE_API bool GetVisible()                           { return ageHook::Thunk<0x4CF070>::Call<bool>(this); }
-        AGE_API void SetVisible(bool a1)                    {ageHook::Thunk<0x4CF050>::Call<void>(this, a1); }
+        AGE_API void SetVisible(bool a1)                    { ageHook::Thunk<0x4CF050>::Call<void>(this, a1); }
         
         /*
             lvlInstance virtuals
         */
 
-        virtual AGE_API void Reset()  override              { ageHook::Thunk<0x4CDFD0>::Call<void>(this); }
-        virtual AGE_API Vector3 const & GetPosition() override
+        AGE_API void Reset()  override                      { ageHook::Thunk<0x4CDFD0>::Call<void>(this); }
+        AGE_API Vector3 const & GetPosition() override
                                                             { return ageHook::Thunk<0x4CEF50>::Call<Vector3 const &>(this); }
-        virtual AGE_API Matrix34 const & GetMatrix(Matrix34 const & a1)
-                                                            { return ageHook::Thunk<0x4CEF90>::Call<Matrix34 const &>(this, a1); }
-        virtual AGE_API void SetMatrix(Matrix34 const & a1) override 
-                                                            { ageHook::Thunk<0x4CEFA0>::Call<void>(this); }
-        virtual AGE_API dgPhysEntity* GetEntity() override  { return ageHook::Thunk<0x4CEFC0>::Call<dgPhysEntity*>(this); }
-        virtual AGE_API dgPhysEntity* AttachEntity() override 
+        AGE_API Matrix34 const & GetMatrix(Matrix34 &a1)
+                                                            { return ageHook::Thunk<0x4CEF90>::Call<Matrix34 const &>(this, &a1); }
+        AGE_API void SetMatrix(Matrix34 const &a1) override 
+                                                            { ageHook::Thunk<0x4CEFA0>::Call<void>(this, &a1); }
+        AGE_API dgPhysEntity* GetEntity() override          { return ageHook::Thunk<0x4CEFC0>::Call<dgPhysEntity*>(this); }
+        AGE_API dgPhysEntity* AttachEntity() override 
                                                             { return ageHook::Thunk<0x4CEFD0>::Call<dgPhysEntity*>(this); }
-        virtual AGE_API Vector3 const * GetVelocity() override
+        AGE_API Vector3 const * GetVelocity() override
                                                             { return ageHook::Thunk<0x4CEF80>::Call<Vector3 const*>(this); }
 
-        virtual AGE_API void Draw(int a1) override          { ageHook::Thunk<0x4CE040>::Call<void>(this, a1); }
-        virtual AGE_API void DrawShadow() override          { ageHook::Thunk<0x4CE940>::Call<void>(this); }
-        virtual AGE_API void DrawShadowMap() override       { ageHook::Thunk<0x4CEA90>::Call<void>(this); }
-        virtual AGE_API void DrawGlow() override            { ageHook::Thunk<0x4CEB90>::Call<void>(this); }
-        virtual AGE_API void DrawReflected(float a1) override
+        AGE_API void Draw(int a1) override                  { ageHook::Thunk<0x4CE040>::Call<void>(this, a1); }
+        AGE_API void DrawShadow() override                  { ageHook::Thunk<0x4CE940>::Call<void>(this); }
+        AGE_API void DrawShadowMap() override               { ageHook::Thunk<0x4CEA90>::Call<void>(this); }
+        AGE_API void DrawGlow() override                    { ageHook::Thunk<0x4CEB90>::Call<void>(this); }
+        AGE_API void DrawReflected(float a1) override
                                                             { ageHook::Thunk<0x4CF080>::Call<void>(this, a1); }
-        virtual AGE_API unsigned int SizeOf() override      { return ageHook::Thunk<0x4CDFE0>::Call<int>(this); }
+        AGE_API unsigned int SizeOf() override              { return ageHook::Thunk<0x4CDFE0>::Call<int>(this); }
 
 
 
@@ -172,12 +172,12 @@ namespace MM2
             asNode virtuals
         */
 
-        virtual AGE_API void Update()  override             {ageHook::Thunk<0x4CC8E0>::Call<void>(this); }
-        virtual AGE_API void Reset()  override              { ageHook::Thunk<0x4CBA70>::Call<void>(this); }
-        virtual AGE_API void FileIO(datParser &parser)  override
+        AGE_API void Update() override                      { ageHook::Thunk<0x4CC8E0>::Call<void>(this); }
+        AGE_API void Reset() override                       { ageHook::Thunk<0x4CBA70>::Call<void>(this); }
+        AGE_API void FileIO(datParser &parser)  override
                                                             { ageHook::Thunk<0x4CCC70>::Call<void>(this); }
-        virtual AGE_API char* GetClassName() override       { return ageHook::Thunk<0x4CCF10>::Call<char*>(this); }
-        virtual AGE_API char const* GetDirName() override   { return ageHook::Thunk<0x4CBAF0>::Call<char const*>(this); }
+        AGE_API char* GetClassName() override               { return ageHook::Thunk<0x4CCF10>::Call<char*>(this); }
+        AGE_API char const* GetDirName() override           { return ageHook::Thunk<0x4CBAF0>::Call<char const*>(this); }
 
         static void BindLua(LuaState L) {
             LuaBinding(L).beginClass<vehCarSim>("vehCarSim")
@@ -213,12 +213,12 @@ namespace MM2
             asNode virtuals
         */
 
-        virtual AGE_API void Update() override              { ageHook::Thunk<0x5B2C30>::Call<void>(this); }
-        virtual AGE_API void Reset() override               { ageHook::Thunk<0x4CAE60>::Call<void>(this); }
-        virtual AGE_API void FileIO(datParser &parser) override
+        AGE_API void Update() override                      { ageHook::Thunk<0x5B2C30>::Call<void>(this); }
+        AGE_API void Reset() override                       { ageHook::Thunk<0x4CAE60>::Call<void>(this); }
+        AGE_API void FileIO(datParser &parser) override
                                                             { ageHook::Thunk<0x4CB400>::Call<void>(this); }
-        virtual AGE_API char* GetClassName() override       { return ageHook::Thunk<0x4CB640>::Call<char*>(this); }
-        virtual AGE_API char const* GetDirName() override   { return ageHook::Thunk<0x4CA5F0>::Call<char const*>(this); }
+        AGE_API char* GetClassName() override               { return ageHook::Thunk<0x4CB640>::Call<char*>(this); }
+        AGE_API char const* GetDirName() override           { return ageHook::Thunk<0x4CA5F0>::Call<char const*>(this); }
         
 
         static void BindLua(LuaState L) {
@@ -267,13 +267,13 @@ namespace MM2
             dgPhysEntity virtuals
         */
 
-        virtual AGE_API bool RequiresTerrainCollision() override
+        AGE_API bool RequiresTerrainCollision() override
                                                             { return ageHook::Thunk<0x42CA90>::Call<bool>(this); }
-        virtual AGE_API lvlInstance* GetInst() override     { return ageHook::Thunk<0x42CA80>::Call<lvlInstance *>(this); }
-        virtual AGE_API phInertialCs* GetICS()              { return ageHook::Thunk<0x42CA70>::Call<phInertialCs *>(this); }
-        virtual AGE_API void PostUpdate() override          { ageHook::Thunk<0x42C8B0>::Call<void>(this); }
-        virtual AGE_API void Update() override              { ageHook::Thunk<0x42C690>::Call<void>(this); }
-        virtual AGE_API void PreUpdate() override           { ageHook::Thunk<0x42C480>::Call<void>(this); }
+        AGE_API lvlInstance* GetInst() override             { return ageHook::Thunk<0x42CA80>::Call<lvlInstance *>(this); }
+        AGE_API phInertialCs* GetICS()                      { return ageHook::Thunk<0x42CA70>::Call<phInertialCs *>(this); }
+        AGE_API void PostUpdate() override                  { ageHook::Thunk<0x42C8B0>::Call<void>(this); }
+        AGE_API void Update() override                      { ageHook::Thunk<0x42C690>::Call<void>(this); }
+        AGE_API void PreUpdate() override                   { ageHook::Thunk<0x42C480>::Call<void>(this); }
 
         static void BindLua(LuaState L) {
             LuaBinding(L).beginClass<vehCar>("vehCar")
