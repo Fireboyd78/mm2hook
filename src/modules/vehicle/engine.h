334c2254f760c48dd8b545b096e4649c7e72224b
Dummiesman <aaro4130@gmail.com>
2019-09-03 16:39:47 -0400

    Added mostly mapped out engine class, and lua alongside it

--- src/modules/vehicle/engine.h
+++ src/modules/vehicle/engine.h
@@ -7,11 +7,39 @@ namespace MM2
     class vehEngine;
 
     // External declarations
-
+    extern class vehCarSim;
+    extern class vehTransmission;
 
     // Class definitions
-
     class vehEngine : public asNode {
+    private:
+        float MaxHorsePower;
+        float IdleRPM;
+        float OptRPM;
+        float MaxRPM;
+        float GCL;
+        float unknown648;
+        int unknown652;
+        float AngInertia;
+        float MaxTorque;
+        float OptTorque;
+        float IdleTorque;
+        float TorqueGap;
+        float unknown676;
+        float sq5add1;
+        float sq5min1;
+        float GearChangedAtRPM;
+        float GCLTimer;
+        BOOL WaitingOnGCL;
+        float ThrottleInput;
+        float CurrentTorque;
+        float CurrentRPM;
+        float unknown712;
+        float ThrottleTorque;
+        Matrix34 *EngineVisualMatrixRef;
+        Matrix34 *EngineVisualMatrixPtr;
+        vehCarSim *VehCarSimPtr;
+        vehTransmission *VehTransmissionPtr;
     public:
 
         AGE_API void ComputeConstants()                     { ageHook::Thunk<0x4D8DC0>::Call<void>(this); }
@@ -30,8 +58,40 @@ namespace MM2
                                                             { ageHook::Thunk<0x4D9240>::Call<void>(this); }
         AGE_API char* GetClassName() override               { return ageHook::Thunk<0x4D9310>::Call<char*>(this); }
 
+        //lua ahelpers
+        inline bool getWaitingOnGCL() {
+            return WaitingOnGCL == TRUE;
+        }
+
+        inline float getThrottleInput() {
+            return ThrottleInput;
+        }
+
+        inline void setThrottleInput(float input) {
+            ThrottleInput = input;
+        }
+
+        inline float getCurrentRPM() {
+            return CurrentRPM;
+        }
+
+        inline void setCurrentRPM(float rpm) {
+            CurrentRPM = rpm;
+        }
+
         static void BindLua(LuaState L) {
             LuaBinding(L).beginExtendClass<vehEngine, asNode>("vehEngine")
+                //properties
+                .addVariableRef("MaxHorsePower", &vehEngine::MaxHorsePower)
+                .addVariableRef("IdleRPM", &vehEngine::IdleRPM)
+                .addVariableRef("OptRPM", &vehEngine::OptRPM)
+                .addVariableRef("MaxRPM", &vehEngine::MaxRPM)
+                .addVariableRef("GCL", &vehEngine::GCL)
+                .addVariableRef("AngInertia", &vehEngine::AngInertia)
+                .addProperty("RPM", &getCurrentRPM, &setCurrentRPM)
+                .addPropertyReadOnly("WaitingOnGCL", &getWaitingOnGCL)
+
+                //functions
                 .addFunction("ComputeConstants", &ComputeConstants)
                 .addFunction("CalcTorqueAtFullThrottle", &CalcTorqueAtFullThrottle)
                 .addFunction("CalcTorqueAtZeroThrottle", &CalcTorqueAtZeroThrottle)
@@ -40,10 +100,8 @@ namespace MM2
 
                 .endClass();
         }
-    private:
-        byte _buffer[0x84];
-
     };
+    ASSERT_SIZEOF(vehEngine, 0x84);
 
     // Lua initialization
 
