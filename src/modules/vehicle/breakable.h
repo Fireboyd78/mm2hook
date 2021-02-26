d11afc9bc2bd6329734658327092bdbd27d36762
Dummiesman <aaro4130@gmail.com>
2021-02-25 20:26:28 -0500

    Fully map out vehBreakable, add support for breakable reflections, rewrite breakable draw code

--- src/modules/vehicle/breakable.h
+++ src/modules/vehicle/breakable.h
@@ -11,13 +11,15 @@ namespace MM2
     extern class modStatic;
     
     class vehBreakable {
-    private:
-        /*0x00*/bool isAttached; // still attached to the parent?
-        /*0x04*/int index; // index into the list of breakables
-
-        /*0x08*/byte _buffer[0x40];
-
-        /*0x48*/vehBreakable *next;
+    public:
+        bool isAttached; // still attached to the parent?
+        int index; // index into the list of breakables
+        Matrix34 matrix;
+        lvlInstance::GeomTableEntry* model;
+        int bangerDataIndex;
+        int geometryIndex;
+        lvlInstance* instance;
+        vehBreakable *next;
     public:
         AGE_API vehBreakable(modStatic **a1, Matrix34 *a2, int a3, int a4, int a5)
                                                             { hook::Thunk<0x4D8530>::Call<void>(this); }
@@ -32,6 +34,8 @@ namespace MM2
     };
 
     class vehBreakableMgr {
+    public:
+        static bool EnableReflections;
     private:
         vehBreakable *first; // linked-list
         float maxImpact; // educated guess (default: 10000.0)
@@ -45,17 +49,68 @@ namespace MM2
         float p7; // (default: 2.0)
         float p8; // (default: 0.0)
     public:
+        inline int getVariant() {
+            return this->variant;
+        }
+
+        inline void setVariant(int variant) {
+            this->variant = variant;
+        }
+
+    public:
+        ANGEL_ALLOCATOR
+
         AGE_API vehBreakableMgr()                           { hook::Thunk<0x4D8580>::Call<void>(this); }
         AGE_API ~vehBreakableMgr()                          { hook::Thunk<0x4D85B0>::Call<void>(this); }
 
+        AGE_API bool Init(Matrix34* a1)                     { return hook::Thunk<0x4D85E0>::Call<bool>(this, a1); }
         AGE_API void Add(LPCSTR a1, LPCSTR a2, modStatic **a3, int a4, int a5)     
                                                             { hook::Thunk<0x4D86A0 >::Call<void>(this, a1, a2, a3, a4, a5); }
         AGE_API void Create(LPCSTR a1, LPCSTR a2, modStatic **a3, int a4, int a5)
                                                             { hook::Thunk<0x4D8600 >::Call<void>(this, a1, a2, a3, a4, a5); }
         AGE_API void Eject(vehBreakable *a1, int room)      { hook::Thunk<0x4D8940>::Call<void>(this, a1, room); }
         AGE_API void EjectAll(int room)                     { hook::Thunk<0x4D8BB0>::Call<void>(this, room); }
+
+
         AGE_API void Draw(const Matrix34* a1, modShader* a2, int a3)
-                                                            { hook::Thunk<0x4D8780>::Call<void>(this, a1, a2, a3); }
+        {
+            for (auto i = this->first; i; i = i->next)
+            {
+                if (i->isAttached)
+                {
+                    auto lodModel = i->model->getLOD(a3);
+                    if (lodModel != nullptr)
+                    {
+                        Matrix34 drawMatrix = Matrix34();
+                        drawMatrix.Set(&i->matrix);
+                        drawMatrix.Dot(a1);
+
+                        //setup renderer
+                        Matrix44::Convert(gfxRenderState::sm_World, &drawMatrix);
+                        gfxRenderState::m_Touched = gfxRenderState::m_Touched | 0x88;
+
+                        lodModel->Draw(a2);
+
+                        //reflection
+                        if (EnableReflections)
+                        {
+                            hook::Type<gfxTexture*> g_ReflectionMap = 0x628914;
+                            bool isSoftware = *(bool*)0x6830D4;
+
+                            auto state = &MMSTATE;
+                            if (a3 == 3 && (g_ReflectionMap != nullptr && !isSoftware && state->EnableReflections))
+                            {
+                                modShader::BeginEnvMap(g_ReflectionMap, drawMatrix);
+                                lodModel->DrawEnvMapped(a2, g_ReflectionMap, 1.f);
+                                modShader::EndEnvMap();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
         AGE_API vehBreakable * Get(int index)               { return hook::Thunk<0x4D86F0>::Call<vehBreakable *>(this, index); }
 
         static void BindLua(LuaState L) {
