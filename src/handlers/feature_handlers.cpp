26845d9be433b89a42f945032cf8152e70363b49
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2021-03-29 21:58:36 +0200

    Added MM1 style damage.

--- src/handlers/feature_handlers.cpp
+++ src/handlers/feature_handlers.cpp
@@ -3437,6 +3437,8 @@ void vehWheelHandler::Install()
     fxTexelDamageHandler
 */
 
+static ConfigValue<bool> cfgMm1StyleDamage("MM1StyleDamage", false);
+
 void fxTexelDamageHandler::Install()
 {
     InstallCallback("fxTexelDamage::ApplyDamage", "",
@@ -3462,6 +3464,42 @@ void fxTexelDamageHandler::Install()
             cb::jmp(0x591CC0)
         }
     );
+
+    if (!cfgMm1StyleDamage.Get())
+        return;
+
+    InstallPatch({ 0x8B, 0x55, 0x0 }, {
+        0x59242D,
+    });
+
+    InstallPatch({ 0x8B, 0x55, 0x0 }, {
+        0x59246C,
+    });
+
+    InstallPatch({ 0x8B, 0x55, 0x0 }, {
+        0x5924A9,
+    });
+
+    InstallPatch({ 0x8B, 0x55, 0x0 }, {
+        0x5924E5,
+    });
+
+    InstallPatch({ 0x8B, 0x55, 0x0 }, {
+        0x592525,
+    });
+
+    InstallPatch({ 0x8B, 0x55, 0x0 }, {
+        0x5925F7,
+    });
+
+    InstallPatch({ 0x8B, 0x45, 0x0 }, {
+        0x592879,
+    });
+
+    //set TexelDamageRadius value to 0.5f
+    InstallPatch({ 0xD9, 0x5, 0x60, 0x3, 0x5B, 0x0 }, {
+        0x4CAE35, // vehCarDamage::Update
+    });
 }
 
 /*
