7835b9e232eceea99600505b4f3ff72f6f1854b8
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2021-04-25 19:21:42 +0200

    Revert back the latest commit.

--- src/handlers/feature_handlers.cpp
+++ src/handlers/feature_handlers.cpp
@@ -3233,12 +3233,6 @@ void mmPlayerHandler::Install() {
             0x405ED4,
         });
     }
-
-    InstallPatch("Makes vpcop use its own vehCarSim file.", { 0xEB }, {
-        0x403BF5, // mmPlayer::Init
-        0x43C558, // mmNetObject::Init
-        0x43C6C4, // mmNetObject::ReInit
-    });
 }
 
 /*
