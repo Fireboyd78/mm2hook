e6d01b5b568014b357e6af2e4455895ff001b4b3
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2021-04-30 04:48:04 +0200

    Implemented cop speech wav sounds for busted feature.

--- src/handlers/feature_handlers.cpp
+++ src/handlers/feature_handlers.cpp
@@ -3022,6 +3022,15 @@ void mmPlayerHandler::BustPerp() {
                     }
                 }
                 if (bustedTimer > bustedTimeout) {
+                    mmGameManager *mgr = mmGameManager::Instance;
+                    auto game = mgr->getGame();
+                    auto soundBase = *getPtr<AudSoundBase*>(game, 0x8C);
+                    int i = rand() % 20 + 1;
+                    string_buf<80> buffer("ACOPAPP%02d%s", i, MMSTATE->CityName);
+                    if (soundBase->Load(buffer, i + 7, false)) {
+                        soundBase->SetSoundHandleIndex(i + 7);
+                        soundBase->PlayOnce(-1.f, -1.f);
+                    }
                     player->getHUD()->SetMessage("Busted!", 4.f, 0);
                     police->StopSiren();
                     AIMAP->policeForce->UnRegisterCop(*getPtr<vehCar*>(police, 0x14), *getPtr<vehCar*>(police, 0x9774));
