923dbfd0adf7826233ed28b4a489c74d2f91c521
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2021-07-29 22:46:21 +0200

    Added asLinearCS class.

--- src/modules/node/linear.h
+++ src/modules/node/linear.h
@@ -4,13 +4,25 @@
 namespace MM2
 {
     // Forward declarations
-
+    class asLinearCS;
 
     // External declarations
 
 
     // Class definitions
+    class asLinearCS : public asNode {
+    public:
+        Matrix34 field_18;
+        Matrix34 field_48;
+        int field_78;
+        int field_7c;
 
+        static void BindLua(LuaState L) {
+            LuaBinding(L).beginExtendClass<asLinearCS, asNode>("asLinearCS")
+                .endClass();
+        }
+    };
+    ASSERT_SIZEOF(asLinearCS, 0x80);
 
     // Lua initialization
 
