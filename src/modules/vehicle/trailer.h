e7c1afbc7cab3e24917435a70026296168664c64
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2021-09-23 21:43:27 +0200

    Fully map out vehTrailer

--- src/modules/vehicle/trailer.h
+++ src/modules/vehicle/trailer.h
@@ -16,36 +16,166 @@ namespace MM2
 
     // Class definitions
 
-    class vehTrailer : public dgPhysEntity, public asNode {
-    protected:
-        hook::Field<0x1E4, vehCarSim *> _sim;
-        hook::Field<0xCC, dgTrailerJoint> _joint;
-        hook::Field<0x528, vehWheel> _twhl0;
-        hook::Field<0x794, vehWheel> _twhl1;
-        hook::Field<0xA00, vehWheel> _twhl2;
-        hook::Field<0xC6C, vehWheel> _twhl3;
+    class vehTrailerInstance : public lvlInstance {
+    private:
+        vehTrailer* Trailer;
+        int field_18;
+        Vector3 TrailerHitchPosition;
     public:
-        inline vehCarSim* getCarSim(void) const {
-            return _sim.get(this);
+        inline vehTrailer * getTrailer(void) {
+            return this->Trailer;
         }
 
-        inline dgTrailerJoint * getJoint(void) const {
-            return _joint.ptr(this);
+        inline Vector3 getTrailerHitchPosition(void) {
+            return this->TrailerHitchPosition;
+        }
+
+        /*
+            lvlInstance virtuals
+        */
+
+        virtual AGE_API Vector3 const & GetPosition() override                         { return hook::Thunk<0x4D7810>::Call<Vector3 const&>(this); }
+        virtual AGE_API Matrix34 const & GetMatrix(Matrix34 *a1) override              { return hook::Thunk<0x4D77F0>::Call<Matrix34 const&>(this, a1); }
+        virtual AGE_API void SetMatrix(const Matrix34 *a1) override                    { hook::Thunk<0x4D77D0>::Call<void>(this, a1); }
+        virtual AGE_API dgPhysEntity * GetEntity() override                            { return hook::Thunk<0x4D7820>::Call<dgPhysEntity*>(this); };
+        virtual AGE_API dgPhysEntity * AttachEntity() override                         { return hook::Thunk<0x4D7830>::Call<dgPhysEntity*>(this); };
+        virtual AGE_API Vector3 const * GetVelocity() override                         { return hook::Thunk<0x4D7840>::Call<Vector3 const*>(this); }
+        virtual AGE_API void Draw(int lod) override                                    { hook::Thunk<0x4D7F20>::Call<void>(this, lod); }
+        virtual AGE_API void DrawShadow() override                                     { hook::Thunk<0x4D81F0>::Call<void>(this); }
+        virtual AGE_API void DrawShadowMap() override                                  { hook::Thunk<0x4D8320>::Call<void>(this); }
+        virtual AGE_API unsigned int SizeOf() override                                 { return hook::Thunk<0x4D7850>::Call<int>(this); };
+
+        static void BindLua(LuaState L) {
+            LuaBinding(L).beginExtendClass<vehTrailerInstance, lvlInstance>("vehTrailerInstance")
+                //properties
+                .addProperty("Trailer", &getTrailer)
+                .addProperty("TrailerHitchPosition", &getTrailerHitchPosition)
+            .endClass();
+        }
+    };
+    ASSERT_SIZEOF(vehTrailerInstance, 0x28);
+
+    class vehTrailer : public dgPhysEntity, public asNode {
+    private:
+        dgTrailerJoint TrailerJoint;
+        vehTrailerInstance TrailerInstance;
+        phInertialCS ICS;
+        float Mass;
+        Vector3 InertiaBox;
+        Vector3 Center;
+        Vector3 CarHitchOffset;
+        Vector3 TrailerHitchOffset;
+        vehDrivetrain DrivetrainFL;
+        vehDrivetrain DrivetrainFR;
+        vehDrivetrain DrivetrainBL;
+        vehDrivetrain DrivetrainBR;
+        vehWheel WheelFL;
+        vehWheel WheelFR;
+        vehWheel WheelBL;
+        vehWheel WheelBR;
+        lvlTrackManager TrackFL;
+        lvlTrackManager TrackFR;
+        lvlTrackManager TrackBL;
+        lvlTrackManager TrackBR;
+    public:
+        inline dgTrailerJoint * getTrailerJoint(void) {
+            return &this->TrailerJoint;
+        }
+
+        inline vehTrailerInstance * getTrailerInstance(void) {
+            return &this->TrailerInstance;
+        }
+
+        inline phInertialCS * getICS(void) {
+            return &this->ICS;
+        }
+
+        inline float getMass(void) {
+            return this->Mass;
+        }
+
+        inline void setMass(float mass) {
+            this->Mass = mass;
+        }
+
+        inline void setAndApplyMass(float mass) {
+            this->setMass(mass);
+
+            auto box = this->getInertiaBox();
+            this->getICS()->InitBoxMass(this->getMass(), box.X, box.Y, box.Z);
+        }
+
+        inline Vector3 getInertiaBox(void) {
+            return this->InertiaBox;
+        }
+
+        inline void setInertiaBox(Vector3 inertiaBox) {
+            this->InertiaBox = inertiaBox;
+        }
+
+        inline void setAndApplyInertiaBox(Vector3 inertiaBox) {
+            this->setInertiaBox(inertiaBox);
+
+            auto box = this->getInertiaBox();
+            this->getICS()->InitBoxMass(this->getMass(), box.X, box.Y, box.Z);
+        }
+
+        inline Vector3 getCenter(void) {
+            return this->Center;
+        }
+
+        inline Vector3 getCarHitchOffset(void) {
+            return this->CarHitchOffset;
+        }
+
+        inline Vector3 getTrailerHitchOffset(void) {
+            return this->TrailerHitchOffset;
+        }
+
+        inline vehDrivetrain * getDrivetrain(int num) {
+            switch (num) {
+            case 0:
+                return &this->DrivetrainFL;
+            case 1:
+                return &this->DrivetrainFR;
+            case 2:
+                return &this->DrivetrainBL;
+            case 3:
+                return &this->DrivetrainBR;
+            }
+            return nullptr;
         }
 
         inline vehWheel * getWheel(int num) {
             switch (num) {
             case 0:
-                return _twhl0.ptr(this);
+                return &this->WheelFL;
             case 1:
-                return _twhl1.ptr(this);
+                return &this->WheelFR;
             case 2:
-                return _twhl2.ptr(this);
+                return &this->WheelBL;
             case 3:
-                return _twhl3.ptr(this);
+                return &this->WheelBR;
             }
             return nullptr;
         }
+
+        inline lvlTrackManager * getTrackManager(int num) {
+            switch (num) {
+            case 0:
+                return &this->TrackFL;
+            case 1:
+                return &this->TrackFR;
+            case 2:
+                return &this->TrackBL;
+            case 3:
+                return &this->TrackBR;
+            }
+            return nullptr;
+        }
+
+        AGE_API vehTrailer()                                            { hook::Thunk<0x4D6F40>::Call<void>(this); }
+        AGE_API ~vehTrailer()                                           { hook::Thunk<0x4D71A0>::Call<void>(this); }
 
         AGE_API void Init(const char* basename, const Vector3* a2, vehCarSim* a3, int a4)
         {
@@ -54,49 +184,46 @@ namespace MM2
             Matrix34 diffMatrix;
 
             if (GetPivot(diffMatrix, basename, "trailer_twhl4")) {
-                getCarSim()->TrailerBackBackLeftWheelPosDiff.X = diffMatrix.m30 - getWheel(2)->getCenter().X;
-                getCarSim()->TrailerBackBackLeftWheelPosDiff.Y = diffMatrix.m31 - getWheel(2)->getCenter().Y;
-                getCarSim()->TrailerBackBackLeftWheelPosDiff.Z = diffMatrix.m32 - getWheel(2)->getCenter().Z;
+                this->getTrailerJoint()->getCarSim()->TrailerBackBackLeftWheelPosDiff.X = diffMatrix.m30 - getWheel(2)->getCenter().X;
+                this->getTrailerJoint()->getCarSim()->TrailerBackBackLeftWheelPosDiff.Y = diffMatrix.m31 - getWheel(2)->getCenter().Y;
+                this->getTrailerJoint()->getCarSim()->TrailerBackBackLeftWheelPosDiff.Z = diffMatrix.m32 - getWheel(2)->getCenter().Z;
             }
 
             if (GetPivot(diffMatrix, basename, "trailer_twhl5")) {
-                getCarSim()->TrailerBackBackRightWheelPosDiff.X = diffMatrix.m30 - getWheel(3)->getCenter().X;
-                getCarSim()->TrailerBackBackRightWheelPosDiff.Y = diffMatrix.m31 - getWheel(3)->getCenter().Y;
-                getCarSim()->TrailerBackBackRightWheelPosDiff.Z = diffMatrix.m32 - getWheel(3)->getCenter().Z;
+                this->getTrailerJoint()->getCarSim()->TrailerBackBackRightWheelPosDiff.X = diffMatrix.m30 - getWheel(3)->getCenter().X;
+                this->getTrailerJoint()->getCarSim()->TrailerBackBackRightWheelPosDiff.Y = diffMatrix.m31 - getWheel(3)->getCenter().Y;
+                this->getTrailerJoint()->getCarSim()->TrailerBackBackRightWheelPosDiff.Z = diffMatrix.m32 - getWheel(3)->getCenter().Z;
             }
         }
 
+        AGE_API int BottomedOut()                                       { return hook::Thunk<0x4D7980>::Call<int>(this); }
+
+        /*
+            dgPhysEntity virtuals
+        */
+
+        virtual AGE_API bool RequiresTerrainCollision() override        { return hook::Thunk<0x4D7870>::Call<bool>(this); }
+        virtual AGE_API void Update() override                          { hook::Thunk<0x4D7B00>::Call<void>(this); }
+
+        /*
+            asNode virtuals
+        */
+
+        virtual AGE_API void Reset() override                           { hook::Thunk<0x4D79C0>::Call<void>(this); }
+        virtual AGE_API void FileIO(datParser &parser) override         { hook::Thunk<0x4D7CB0>::Call<void>(this, &parser); }
+
         static void BindLua(LuaState L) {
             LuaBinding(L).beginExtendClass<vehTrailer, dgPhysEntity>("vehTrailer")
-                .addPropertyReadOnly("vehCarSim", &getCarSim)
-                .addPropertyReadOnly("Joint", &getJoint)
-            .endClass();
+                .addProperty("Mass", &getMass, &setAndApplyMass)
+                .addProperty("InertiaBox", &getInertiaBox, &setAndApplyInertiaBox)
+                .addFunction("GetDrivetrain", &getDrivetrain)
+                .addFunction("GetWheel", &getWheel)
+                .addFunction("GetTrackManager", &getTrackManager)
+                .addFunction("BottomedOut", &BottomedOut)
+                .endClass();
         }
     };
-
-    class vehTrailerInstance : public lvlInstance {
-    private:
-        vehTrailer* Trailer;
-        int field_18;
-        Vector3 TrailerHitchPosition;
-    public:
-        inline vehTrailer * getTrailer(void) const {
-            return this->Trailer;
-        }
-
-        inline Vector3 getTrailerHitchPosition(void) {
-            return this->TrailerHitchPosition;
-        }
-
-        static void BindLua(LuaState L) {
-            LuaBinding(L).beginExtendClass<vehTrailerInstance, lvlInstance>("vehTrailerInstance")
-                //properties
-                .addPropertyReadOnly("Trailer", &getTrailer)
-                .addProperty("TrailerHitchPosition", &getTrailerHitchPosition)
-            .endClass();
-        }
-    };
-    ASSERT_SIZEOF(vehTrailerInstance, 0x28);
+    ASSERT_SIZEOF(vehTrailer, 0x1038);
 
     // Lua initialization
 
