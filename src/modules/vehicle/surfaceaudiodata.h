8ac9b2bead1637ee71b8fdb84daeb3b7b7dc88bf
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2021-10-10 02:39:38 +0200

    Add vehSurfaceAudioData

--- src/modules/vehicle/surfaceaudiodata.h
+++ src/modules/vehicle/surfaceaudiodata.h
@@ -4,13 +4,31 @@
 namespace MM2
 {
     // Forward declarations
-
+    class vehSurfaceAudioData;
 
     // External declarations
 
 
     // Class definitions
 
+    class vehSurfaceAudioData {
+    private:
+        AudSoundBase* SurfaceSoundBasePtr;
+        void* SkidWaveData;
+        int SampleIndex;
+        int NumSkidSamples;
+        float MaxSpeed;
+        float MinSurfaceVolume;
+        float MaxSurfaceVolume;
+        float field_1c;
+        float MinSkidVolume;
+        float MaxSkidVolume;
+        float field_28;
+        float MinSurfacePitch;
+        float MaxSurfacePitch;
+        float field_34;
+    };
+    ASSERT_SIZEOF(vehSurfaceAudioData, 0x38);
 
     // Lua initialization
 
