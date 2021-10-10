69d1692d935d768b9264d32e33540a8554c68ead
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2021-10-10 02:38:10 +0200

    Add vehEngineSampleWrapper

---
+++ src/modules/vehicle/enginesamplewrapper.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <modules\vehicle.h>
+
+namespace MM2
+{
+    // Forward declarations
+    class vehEngineSampleWrapper;
+
+    // External declarations
+
+
+    // Class definitions
+
+    class vehEngineSampleWrapper {
+    private:
+        AudSoundBase* SoundBasePtr;
+        float MinVolume;
+        float MaxVolume;
+        float MinVolumeBackup;
+        float MaxVolumeBackup;
+        int field_14;
+        int field_18;
+        float FadeInStartRPM;
+        float FadeOutStartRPM;
+        float FadeInEndRPM;
+        float FadeOutEndRPM;
+        float MinPitch;
+        float MaxPitch;
+        float PitchShiftStartRPM;
+        float PitchShiftEndRPM;
+        float PitchDivisor;
+        bool EchoOn;
+        bool UseOldMode;
+        byte gap2;
+        byte gap3;
+        float VolumeDivisor;
+        float VolInverseRpm;
+        int SoundIndex;
+    };
+    ASSERT_SIZEOF(vehEngineSampleWrapper, 0x50);
+
+    // Lua initialization
+
+}
