17034a9afed6232a55fb5e14913a607ad0359839
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2021-10-10 02:41:25 +0200

    Add vehSurfaceAudio

--- src/modules/vehicle/surfaceaudio.h
+++ src/modules/vehicle/surfaceaudio.h
@@ -1,16 +1,54 @@
 #pragma once
 #include <modules\vehicle.h>
+#include <mm2_audio.h>
 
 namespace MM2
 {
     // Forward declarations
-
+    class vehSurfaceAudio;
 
     // External declarations
-
+    extern class vehSurfaceAudioData;
 
     // Class definitions
 
+    class vehSurfaceAudio {
+    private:
+        int LastSurfaceSound;
+        int field_4;
+        int IsPlayingSkidSound;
+        int SurfaceDataCount;
+        int field_10;
+        int TireWobbleSampleIndex;
+        int field_18;
+        int field_1c;
+        float MaxSuspensionVolume;
+        float MinSuspensionVolume;
+        float VolumeMultiplier;
+        float MinSuspensionVelocity;
+        float MaxSuspensionVelocity;
+        int field_34;
+        float TireWobbleMinVolume;
+        float TireWobbleMaxVolume;
+        float TireWobbleMinPitch;
+        float TireWobbleMaxPitch;
+        float TireWobblePitchDivisor;
+        float field_4c;
+        float field_50;
+        int field_54;
+        int field_58;
+        vehSurfaceAudioData** SurfaceDatas;
+        AudSoundBase* SuspensionSound;
+        AudSoundBase* WobbleSoundPtr;
+        vehWheel* WHL0;
+        vehWheel* WHL1;
+        vehWheel* WHL2;
+        vehWheel* WHL3;
+        Vector3* PositionPtr;
+        vehCarDamage* CarDamage;
+        int field_80;
+    };
+    ASSERT_SIZEOF(vehSurfaceAudio, 0x84);
 
     // Lua initialization
 
