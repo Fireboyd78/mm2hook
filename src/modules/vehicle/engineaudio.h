b272a5a8e8460cc86ed99e97c71f10098af98500
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2021-10-10 02:42:00 +0200

    Add vehEngineAudio

--- src/modules/vehicle/engineaudio.h
+++ src/modules/vehicle/engineaudio.h
@@ -4,13 +4,19 @@
 namespace MM2
 {
     // Forward declarations
-
+    class vehEngineAudio;
 
     // External declarations
-
+    extern class vehEngineSampleWrapper;
 
     // Class definitions
 
+    class vehEngineAudio {
+    private:
+        int SampleCount;
+        vehEngineSampleWrapper** Samples;
+    };
+    ASSERT_SIZEOF(vehEngineAudio, 8);
 
     // Lua initialization
 
