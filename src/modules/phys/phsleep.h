6a18c82d03060797d34b279b76eb86634d0d7475
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2022-02-02 00:01:10 +0200

    Add phSleep definition

--- src/modules/phys/phsleep.h
+++ src/modules/phys/phsleep.h
@@ -1,16 +1,44 @@
 #pragma once
-#include <modules\phys.h>
+#include "phinertia.h"
 
 namespace MM2
 {
     // Forward declarations
-
+    class phSleep;
 
     // External declarations
 
 
     // Class definitions
+    enum class phSleepState : int
+    {
+        ASLEEP = 0x0,
+        AWAKE = 0x1,
+        UNK = 0x2,
+    };
 
+
+    class phSleep
+    {
+    private:
+        int dword_00;
+        int dword_04;
+        int dword_08;
+        Vector3 dword_0c;
+        Vector3 LastAcceleration;
+        phInertialCS* ICS;
+        int Collider;
+        int dword_2c;
+        int dword_30;
+        int AwakeFrameCount;
+        int MaxAwakeFrames;
+        float SomeMagTrigger;
+        float AccelMagThreshold;
+        float NextAwakeTime;
+        phSleepState State;
+        datCallback* AsleepCB;
+    };
+    ASSERT_SIZEOF(phSleep, 0x50);
 
     // Lua initialization
 
