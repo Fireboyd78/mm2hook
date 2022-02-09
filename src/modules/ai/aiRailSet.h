54f81940bfb42a70ac9b549f7761fd395231fe73
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2022-02-10 01:20:10 +0200

    Add aiRailSet

--- src/modules/ai/aiRailSet.h
+++ src/modules/ai/aiRailSet.h
@@ -4,13 +4,51 @@
 namespace MM2
 {
     // Forward declarations
-
+    class aiRailSet;
 
     // External declarations
 
 
     // Class definitions
-
+    class aiRailSet {
+    private:
+        float BackBumperDist;
+        float FrontBumperDist;
+        float LSideDist;
+        float RSideDist;
+        short EnterInt;
+        float RoadDist;
+        float ResumeDist;
+        float SubSectionDist;
+        float TurnDist;
+        float LaneRandomness;
+        short WaitCount;
+        short SSIdx;
+        short CurLane;
+        short NextLane;
+        short TargetLane;
+        int RailType;
+        aiPath* NextLink;
+        aiPath* CurLink;
+        short field_40;
+        short field_42;
+        float CurAccelFactor;
+        float ExheedLimit;
+        float TargetVelocity;
+        float CarReactDist;
+        float VehicleAccelFactor;
+        float SeparationDist;
+        float IntersectionReactDist;
+        float field_60;
+        float field_64;
+        float field_68;
+        float field_6C;
+        float field_70;
+        float field_74;
+        float field_78;
+        float field_7C;
+    };
+    ASSERT_SIZEOF(aiRailSet, 0x80);
 
     // Lua initialization
 
