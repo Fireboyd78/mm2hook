e3ddc26e3e9c1f8f46291fd74a09b8a5d0a28cb6
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2022-02-13 01:41:10 +0200

    Add phPolygon

--- src/modules/phys/phpoly.h
+++ src/modules/phys/phpoly.h
@@ -4,12 +4,62 @@
 namespace MM2
 {
     // Forward declarations
-
+    struct phPolygon;
 
     // External declarations
 
 
     // Class definitions
+    struct phPolygon
+    {
+        Vector3 Normal;
+        float Radius;
+        Vector4 EdgeNormalCross[4];
+        ushort Indices[4];
+        ushort Indices2[4];
+
+        /*
+            Lua Helpers
+        */
+        short getIndex(int i)
+        {
+            if (i < 0 || i >= 4)
+                return -1;
+            return (short)this->Indices[i];
+        }
+
+        void setIndex(int i, ushort index)
+        {
+            if (i >= 0 && i < 4)
+                this->Indices[i] = index;
+        }
+
+        inline byte getMaterialIndex()
+        {
+            return *(byte*)&Radius;
+        }
+
+        inline void setMaterialIndex(byte index)
+        {
+            *(byte*)&Radius = index;
+        }
+
+        inline bool isQuad()
+        {
+            return this->Indices[3] != 0;
+        }
+
+        static void BindLua(LuaState L) {
+            LuaBinding(L).beginClass<phPolygon>("phPolygon")
+                .addPropertyReadOnly("IsQuad", &isQuad)
+                .addVariableRef("Radius", &phPolygon::Radius)
+                .addVariableRef("Normal", &phPolygon::Normal)
+                .addProperty("MaterialIndex", &getMaterialIndex, &setMaterialIndex)
+                .addFunction("GetIndex", &getIndex)
+                .addFunction("SetIndex", &setIndex)
+                .endClass();
+        }
+    };
 
 
     // Lua initialization
