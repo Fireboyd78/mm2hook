962430cf1a0211215b88d174a81e45dac4110223
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2022-02-13 01:42:13 +0200

    Add phIntersectionPoint

--- src/modules/phys/phisectpoint.h
+++ src/modules/phys/phisectpoint.h
@@ -4,14 +4,31 @@
 namespace MM2
 {
     // Forward declarations
-    class phIntersectionPoint;
+    struct phIntersectionPoint;
 
     // External declarations
 
 
     // Class definitions
 
-    class phIntersectionPoint {};
+    struct phIntersectionPoint {
+    public:
+        Vector3 Point;
+        Vector3 Normal;
+        float NormalizedDistance;
+        float Penetration;
+        byte IntersectResult;
+    public:
+        void Set(const Vector3& point, const Vector3& normal, float normalizedDistance, float penetration, bool result)
+        {
+            this->Point = point;
+            this->Normal = normal;
+            this->NormalizedDistance = normalizedDistance;
+            this->Penetration = penetration;
+            this->IntersectResult = result;
+        }
+    };
+    ASSERT_SIZEOF(phIntersectionPoint, 0x24);
 
 
     // Lua initialization
