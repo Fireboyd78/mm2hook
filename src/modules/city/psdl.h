ef1b6b69e2e0ae590a606a6806ec0bcbc4a46f78
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2022-02-13 01:50:51 +0200

    Add sdlPolyCached and sdlPoly

--- src/modules/city/psdl.h
+++ src/modules/city/psdl.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <modules\phys.h>
 #include <modules\city.h>
 
 namespace MM2
@@ -12,6 +13,15 @@ namespace MM2
     // Class definitions
 
     class lvlSDL {};
+
+    struct sdlPoly : phPolygon {};
+
+    struct sdlPolyCached : public sdlPoly
+    {
+        Vector3 Verts[4];
+    };
+    ASSERT_SIZEOF(sdlPolyCached, 0x90);
+
 
     // Lua initialization
 
